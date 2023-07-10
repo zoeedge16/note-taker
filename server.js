ae0007c1fd21a615949cefc872814181c1d03668
Zoe  Edge <zoemedge@Zoes-MacBook-Pro.local>
2023-07-10 12:05:28 -0500

    fixed all the little errors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 // generate unique ID's for the notes
 const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT ?? 3001;
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-const notesFilePath = path.join(__dirname, 'db', 'notes.json')
+const notesFilePath = path.join(__dirname, 'db', 'db.json')
 
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/notes.html'));
@@ -30,7 +31,15 @@ app.post('/api/notes', (req,res) => {
     notes.push(newNote);
     saveNotesToDB(notes);
     return res.json(newNote);
-})
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    let notes = getNotesFromDB();
+    notes = notes.filter((note) => note.id !== id);
+    saveNotesToDB(notes);
+    return res.json({ message: 'Note deleted successfully.'});
+});
 
 app.get('*', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -39,4 +48,13 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Application is running @ http://localhost:${PORT}`);
 });
+
+function getNotesFromDB() {
+    const notesData = fs.readFileSync(notesFilePath, 'utf-8');
+    return JSON.parse(notesData) || [];
+};
+
+function saveNotesToDB(notes) {
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes));
+};
 
