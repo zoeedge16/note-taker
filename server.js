459a45fd17bf76b02ac425697111a8f8f33f5bcb
Zoe  Edge <zoemedge@Zoes-MacBook-Pro.local>
2023-07-10 10:48:37 -0500

    adding code to retrieve the notes.html and getting it to save and fetch the notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+// generate unique ID's for the notes
 const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT ?? 3001;
 const app = express();
@@ -7,9 +8,28 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+const notesFilePath = path.join(__dirname, 'db', 'notes.json')
 
 app.get('/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const notes = getNotesFromDB();
+    return res.json(notes);
+});
+
+app.post('/api/notes', (req,res) => {
+    const { title, text } = req.body;
+    const newNote = {
+        id: uuidv4(),
+        title, 
+        text,
+    };
+    const notes = getNotesFromDB();
+    notes.push(newNote);
+    saveNotesToDB(notes);
+    return res.json(newNote);
 })
 
 app.get('*', (req, res) => {
